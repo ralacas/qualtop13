a22fd4d723a578050af0b78c11339a348642d38b
Omar Mejia <topesa@gmail.com>
2020-08-24 23:36:50 -0700

    Update account_payment.py

--- qualtop_account/models/account_payment.py
+++ qualtop_account/models/account_payment.py
@@ -7,7 +7,7 @@ class AccountPayment(models.Model):
     _inherit = 'account.payment'
 
     def _l10n_mx_edi_sign(self):
-        acc_move = self.env['account.move'].search([('name', '=', self.communication)])
+        acc_move = self.env['account.move'].search([('name', '=', self.communication),('state','!=', 'cancel')])
         if acc_move.not_sign_complement:
             self.message_post(
                 body=_('Este complemento no se timbrará')
