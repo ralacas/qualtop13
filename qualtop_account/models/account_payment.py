d4be337bfda92aea75914de37c0c953139e1bda2
omar <topesa@gmail.com>
2020-08-31 00:14:49 -0700

    [FIX] ignora el CFDI para complemento de pago siempre y cuando tenga activada la casilla de 'No timbrar, o no timbrar complemento'

--- qualtop_account/models/account_payment.py
+++ qualtop_account/models/account_payment.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, _
+from odoo.exceptions import UserError
 
 
 class AccountPayment(models.Model):
@@ -14,3 +15,13 @@ class AccountPayment(models.Model):
             )
             return True
         return super(AccountPayment, self)._l10n_mx_edi_sign()
+
+    def l10n_mx_edi_is_required(self):
+        self.ensure_one()
+        acc_move = self.env['account.move'].search([('name', '=', self.communication), ('state', '!=', 'cancel')])
+        if len(acc_move) > 1:
+            raise UserError('Lo siento, hay dos facturas con el Folio : {}  , favor de contactar con el administrador'.format(self.communication))
+
+        if acc_move.not_sign_complement or acc_move.not_sign:
+            return True
+        return super(AccountPayment, self).l10n_mx_edi_is_required()
