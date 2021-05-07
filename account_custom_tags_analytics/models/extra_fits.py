5eabbe81e8a7f23f4e464f1465f6141272a3cdc2
Germán Ponce Dominguez <german.ponce@argil.mx>
2021-05-07 14:34:18 -0500

    Corrección Conversión de Moneda

--- account_custom_tags_analytics/models/extra_fits.py
+++ account_custom_tags_analytics/models/extra_fits.py
@@ -169,9 +169,7 @@ class AccountAnalyticTagSegmentationWizard(models.TransientModel):
             self.subtotal = self.invoice_id.amount_untaxed
         else:
             total_comp_curr = invoice_currency._convert(self.invoice_id.amount_untaxed, company_currency, company, self.invoice_id.invoice_date)
-            subtotal_currency = self.invoice_id.currency_id._convert(abs(self.invoice_id.amount_untaxed), self.invoice_id.currency_id, self.invoice_id.company_id, self.invoice_id.date)
             _logger.info("\n######### total_comp_curr >>>>>>>>> %s" % total_comp_curr)
-            _logger.info("\n######### subtotal_currency >>>>>>>>> %s" % subtotal_currency)
             # ctx = dict(self._context, date=self.invoice_id.invoice_date)
             # compute_currency = company_currency.with_context(ctx).compute(self.invoice_id.amount_untaxed, invoice_currency)
             self.subtotal = total_comp_curr
